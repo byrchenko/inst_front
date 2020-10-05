304e71e12017b051ff8b4418c612fe04d168c5e4
D. Byrchenko <byrchenko@gmail.com>
2020-10-05 15:15:53 +0300

    initial

---
+++ src/Feed/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./index.scss";
+
+const Feed = (props) => {
+    return (
+        <div className={css.index}>
+            {"Here is feed!"}
+        </div>
+    )
+}
+
+export default Feed;
